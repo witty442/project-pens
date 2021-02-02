658161d74d361adb10fe5b0880e49c0f3623fc1d
witty442 <witty442@gmail.com>
2021-02-02 10:35:21 +0700

    # WARNING: head commit changed in the meantime
    init project

--- salesapp/src/com/pens/util/GoogleMapJavaScriptAPI.java
+++ salesapp/src/com/pens/util/GoogleMapJavaScriptAPI.java
@@ -4,10 +4,11 @@ package com.pens.util;
 public class GoogleMapJavaScriptAPI {
     private static GoogleMapJavaScriptAPI _instance;
     private  String API_KEY = "";
-	
+   // AIzaSyAJUWErumNlehQ6rT4rP52yThSNsYd7bsI
+    
     public GoogleMapJavaScriptAPI(){
     	try{
-    		API_KEY = "AIzaSyAJUWErumNlehQ6rT4rP52yThSNsYd7bsI";
+    		API_KEY = EnvProperties.getInstance().getProperty("google.map.api.key");
 		}catch(Exception e){
 		}
     }
